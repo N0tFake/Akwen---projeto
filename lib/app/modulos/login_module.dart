440cf5c856bc04f9c1748637af455891115f1e5f
Otavio <silvio76otavio@gmail.com>
2021-10-17 15:15:04 -0300

    :construction: | crate login page

---
+++ lib/app/modulos/login_module.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_akwen/app/modulos/login/login_page.dart';
+import 'package:flutter_akwen/app/modulos/login_store.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class LoginModule extends Module {
+
+  static LoginStore loginStore = LoginStore();
+
+  @override
+  final List<Bind> binds = [
+    Bind.lazySingleton((i) => loginStore),
+  ];
+
+  @override
+  final List<ModularRoute> routes = [
+    ChildRoute(Modular.initialRoute, child: (_, args) => const LoginPage()),
+  ];
+}
